54f5e149efd6a300571a9e5da43b739b67f46148
Ivan Tihonov <ivan.tixonov000@gmail.com>
2024-10-31 00:42:11 +0700

    form

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.input-container input').forEach(input => {
+    input.addEventListener('input', () => {
+        if (input.value.length > 0) {
+            input.classList.add('filled');
+            input.classList.add('error');
+        } else {
+            input.classList.remove('filled');
+        }
+    });
+});
+
+const modal = document.getElementById('modal');
+const form = document.querySelector('form');
+console.log(form);
+
+document.getElementById('reg').addEventListener('click', () => {
+    modal.style.opacity = '1';
+    modal.style.pointerEvents = 'auto';
+    form.style.transform = 'scale(1)';
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.opacity = '';
+        modal.style.pointerEvents = ''
+        form.style.transform = '';
+    }
+});
